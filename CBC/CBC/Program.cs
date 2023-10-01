bd774322cc36eba4427a1a7b44a9ea02fa4c0bbb
ITSGABAY <99762916+ITSGABAY@users.noreply.github.com>
2023-10-01 23:32:15 +0300

    Decrypt Finished

--- CBC/CBC/Program.cs
+++ CBC/CBC/Program.cs
@@ -4,26 +4,26 @@ using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using EncryptionAES;
+using AdvancedEncryptionStandard;
 namespace CBC
 {
     internal class Program
     {
-
-
-
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             byte[] dataBlock = {
-                0x54, 0x77, 0x6F, 0x20, 0x4F, 0x6E, 0x65, 0x20,
-                0x4E, 0x69, 0x6E, 0x65, 0x20, 0x54, 0x77, 0x6F
-            };
+    0x54, 0x77, 0x6F, 0x20, 0x4F, 0x6E, 0x65, 0x20,
+    0x4E, 0x69, 0x6E, 0x65, 0x20, 0x54, 0x77, 0x6F
+};
             byte[] Key = {
-                0x54, 0x68, 0x61, 0x74, 0x73, 0x20, 0x6D, 0x79,
-                0x20, 0x4B, 0x75, 0x6E, 0x67, 0x20, 0x46, 0x75
-            };
+    0x54, 0x68, 0x61, 0x74, 0x73, 0x20, 0x6D, 0x79,
+    0x20, 0x4B, 0x75, 0x6E, 0x67, 0x20, 0x46, 0x75
+};
 
-            dataBlock = AES_Encryption.encrypt(dataBlock, Key);
+            dataBlock = AES.Encrypt(dataBlock, Key);
+            
+            dataBlock = AES.Decrypt(dataBlock, Key);
+            Console.WriteLine("");
 
             foreach (Byte b in dataBlock)
             {
@@ -33,6 +33,9 @@ namespace CBC
             Console.ReadKey();
 
 
+
         }
+
+
     }
 }
