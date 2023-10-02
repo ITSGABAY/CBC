bd069d46143031d9b3c554012601d42f773d456c
ITSGABAY <99762916+ITSGABAY@users.noreply.github.com>
2023-10-02 23:59:33 +0300

    CBC FINISHED  , FORM DESIGN FINISHED

---
+++ CBC/CBC/CBC.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CipherBlockChaining
+{
+    internal class CBC
+    {
+        public static byte[,] Decrypt(byte[,] blocks, byte[] key, byte[] IV)
+        {
+            byte[] prevBlock = new byte[16];
+            byte[] newBlock = new byte[16];
+            byte[,] newBlocks = new byte[blocks.GetLength(0), blocks.GetLength(1)];
+            byte[] currentBlock = new byte[16];
+            for (int i = blocks.GetLength(0) - 1; i >= 0; i--)
+            {
+                for (int j = 0; j < 16; j++)
+                {
+                    currentBlock[j] = blocks[i, j];
+                }
+                currentBlock = AES.Decrypt(currentBlock, key);
+                if (i == 0)
+                {
+                    prevBlock = IV;
+                }
+                else
+                {
+                    for (int j = 0; j < 16; j++)
+                    {
+                        prevBlock[j] = blocks[i - 1, j];
+                    }
+                }
+                currentBlock = xorArray(prevBlock, currentBlock);
+                for (int j = 0; j < 16; j++)
+                {
+                    newBlocks[i, j] = currentBlock[j];
+                }
+            }
+            return newBlocks;
+        }
+
+        public static byte[,] Encrypt(byte[,] blocks, byte[] key, byte[] IV)
+        {
+            byte[] prevBlock = IV;
+            byte[] newBlock = new byte[16];
+            byte[,] newBlocks = new byte[blocks.GetLength(0), blocks.GetLength(1)];
+            byte[] currentBlock = new byte[16];
+            for (int i = 0; i < blocks.GetLength(0); i++)
+            {
+                for (int j = 0; j < 16; j++)
+                {
+                    currentBlock[j] = blocks[i, j];
+                }
+                newBlock = AES.Encrypt(xorArray(prevBlock, currentBlock), key);
+                for (int j = 0; j < 16; j++)
+                {
+                    newBlocks[i, j] = newBlock[j];
+                }
+                prevBlock = newBlock;
+            }
+            return newBlocks;
+        }
+
+    }
+}
--- CBC/CBC/Program.cs
+++ CBC/CBC/Program.cs
@@ -11,7 +11,11 @@ namespace CBC
     {
         public static void Main(string[] args)
         {
-            byte[] dataBlock = {
+            byte[] Block1 = {
+    0x54, 0x77, 0x6F, 0x20, 0x4F, 0x6E, 0x65, 0x20,
+    0x4E, 0x69, 0x6E, 0x65, 0x20, 0x54, 0x77, 0x6F
+};
+            byte[] Block2 = {
     0x54, 0x77, 0x6F, 0x20, 0x4F, 0x6E, 0x65, 0x20,
     0x4E, 0x69, 0x6E, 0x65, 0x20, 0x54, 0x77, 0x6F
 };
@@ -19,20 +23,60 @@ namespace CBC
     0x54, 0x68, 0x61, 0x74, 0x73, 0x20, 0x6D, 0x79,
     0x20, 0x4B, 0x75, 0x6E, 0x67, 0x20, 0x46, 0x75
 };
+            byte[] IV = {
+    0x54, 0x68, 0x61, 0x74, 0x73, 0x20, 0x6D, 0x79,
+    0x20, 0x4B, 0x75, 0x6E, 0x67, 0x20, 0x46, 0x75
+};
+            byte[,] Block3 = stringToBlocks("hello my bro icacasdasdasdas");
 
-            dataBlock = AES.Encrypt(dataBlock, Key);
-            
-            dataBlock = AES.Decrypt(dataBlock, Key);
-            Console.WriteLine("");
+            byte[,] newBlocks = encrypt(Block3, Key, IV);
 
-            foreach (Byte b in dataBlock)
+            byte[,] block3 = decrypt(newBlocks, Key, IV);
+
+
+            foreach (Byte b in newBlocks)
             {
-                Console.Write($"0x{b:X2} ");
+                Console.Write($"{b:X2}");
+
             }
+
+            
 
             Console.ReadKey();
 
 
+
+        }
+
+        
+
+        public static byte[,] stringToBlocks(string str)
+        {
+            byte[,] blocks = new byte[(str.Length-1) / 16 +1, 16];
+            int countForPadding = (16 * (str.Length / 16 + 1)) - str.Length;
+            for(int i=0; i<str.Length; i++)
+            {
+                blocks[i/16 , i%16] = Convert.ToByte(str[i]);
+            }
+            for (int i = 0; i <16; i++)
+            {
+                if (blocks[(str.Length-1) / 16, i] == 0x00)
+                {
+                    
+                    blocks[str.Length / 16 , i] = Convert.ToByte(countForPadding);
+                }
+            }
+            return blocks;
+        }
+
+        public static byte[] xorArray(byte[] a1, byte[] a2)
+        {
+            byte[] newArray = new byte[a1.Length];
+            for(int i=0; i<a1.Length;i++)
+            {
+                newArray[i] = (byte)(a1[i] ^ a2[i]);
+            }
+            return newArray;
 
         }
 
